a42658e5d246f30febe7f4f3388bd2977d51f28d
Tarashor <taras@frescano.se>
2024-08-20 08:01:47 +0200

    twitter bot

--- src/main/kotlin/com/tarashor/reposter/TelegramBot.kt
+++ src/main/kotlin/com/tarashor/reposter/TelegramBot.kt
@@ -3,36 +3,34 @@ package com.tarashor.reposter
 import com.github.kotlintelegrambot.bot
 import com.github.kotlintelegrambot.dispatch
 import com.github.kotlintelegrambot.dispatcher.channel
-import com.github.kotlintelegrambot.dispatcher.text
+import com.github.kotlintelegrambot.entities.TelegramFile
 import org.springframework.boot.CommandLineRunner
 import org.springframework.stereotype.Component
 import twitter4j.StatusUpdate
-import twitter4j.TwitterFactory
-import twitter4j.conf.ConfigurationBuilder
+import twitter4j.Twitter
+import java.io.ByteArrayInputStream
 import java.io.File
+import java.io.InputStream
 
 @Component
-class TelegramBot : CommandLineRunner{
+class TelegramBot(
+    private val twitter: Twitter
+) : CommandLineRunner{
     override fun run(vararg args: String?) {
         val bot = bot {
             token = "7229516327:AAG8FoDMXutlYwhih-lHThp_hd92bPDS8qY"
             dispatch {
                 channel {
-                    val photo = channelPost.photo?.last()
-                    val caption = channelPost.caption ?: ""
 
-                    println(photo)
+                    channelPost.photo?.last()?.fileId?.let { fileId ->
+                        val caption = channelPost.caption ?: ""
 
-                    bot.downloadFileBytes(photo!!.fileId)
-                    // Download the photo
-                    val photoFile = downloadPhoto(photo!!.fileId)
+                        val photoFile = ByteArrayInputStream(bot.downloadFileBytes(fileId))
 
-                    // Post to Twitter
-                    postToTwitter(photoFile, caption)
-
-                    // Post to Instagram
-                    postToInstagram(photoFile, caption)
-
+                        println("uploaded - $fileId")
+                        // Post to Twitter
+                        postToTwitter(photoFile, caption)
+                    }
                 }
             }
         }
@@ -46,27 +44,19 @@ class TelegramBot : CommandLineRunner{
         return File(".")
     }
 
-    fun postToTwitter(photo: File, caption: String) {
-//        val cb = ConfigurationBuilder()
-//        cb.setDebugEnabled(true)
-//            .setOAuthConsumerKey("YOUR_CONSUMER_KEY")
-//            .setOAuthConsumerSecret("YOUR_CONSUMER_SECRET")
-//            .setOAuthAccessToken("YOUR_ACCESS_TOKEN")
-//            .setOAuthAccessTokenSecret("YOUR_ACCESS_TOKEN_SECRET")
-//
-//        val tf = TwitterFactory(cb.build())
-//        val twitter = tf.instance
-//
-//        val status = twitter.updateStatus(StatusUpdate(caption).media(photo))
-//        println("Successfully updated Twitter status: ${status.text}")
+    fun postToTwitter(photo: InputStream, caption: String) {
+
+
+        val statueUpdate = StatusUpdate(caption).apply {  setMedia("photo", photo) }
+        val status = twitter.updateStatus(statueUpdate)
+
+        println("Successfully updated Twitter status: ${status.text}")
     }
 
     fun postToInstagram(photo: File, caption: String) {
         // Note: Instagram doesn't have an official API for posting.
         // You might need to use a third-party library or service.
         // This is a placeholder function to represent the concept.
-
-
     }
 
 }
